369c8d9c5e920742a04d8e8f7a67a05554c8c8d0
Will Robertson <will.robertson@powershop.co.nz>
2015-01-21 16:28:47 +1300

    Initial working version

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in squash_repeater-ruby.gemspec
+gemspec
---
+++ lib/squash_repeater.rb
@@ -0,0 +1,14 @@
+# Configuration methods are in `configure.rb`
+# Exception capture and re-run are in `exception_queue.rb`
+
+module SquashRepeater
+  class Error < RuntimeError; end
+end
+
+require "squash_repeater/version"
+require "squash_repeater/configure"
+require "squash_repeater/exception_queue"
+require "squash_repeater/squash_ruby"
+
+# For Rails generators (only if Rails is used):
+require "generators/squash_repeater/install_generator" if defined? Rails
