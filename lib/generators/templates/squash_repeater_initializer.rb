52aa507e389efd4d98619a7b7bbb00d9f4114d3d
mexisme <wildjim+github.com@kiwinet.org>
2015-03-26 12:48:03 +1300

    Add Rails generator for initializer

---
+++ lib/generators/templates/squash_repeater_initializer.rb
@@ -0,0 +1,18 @@
+require "squash_repeater/ruby"
+
+SquashRepeater::Ruby.configure do |c|
+  # The nature of SquashRepeater is that a tiny local queueing system
+  # captures the Squash notification, and retransmits it from a worker.
+  # Therefore, we assume beanstalkd is running locally:
+
+  # c.queue_host = "localhost"
+  # c.namespace = "squash-repeater"
+
+  # You can set Squash::Ruby config here, or through their configration method:
+  # Squash::Ruby api_host:
+  c.squash_url = "http://no.where"
+  # Squash::Ruby api_key:
+  c.squash_key = "12345"
+  #Squash::Ruby environment:
+  c.environment = Rails.env
+end
