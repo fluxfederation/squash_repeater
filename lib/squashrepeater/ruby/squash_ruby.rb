1538c5a5227db59b2e2b7d750340d99e30a560d9
mexisme <wildjim+github.com@kiwinet.org>
2015-03-31 20:58:16 +1300

    Clean-up

--- lib/squashrepeater/ruby/squash_ruby.rb
+++ lib/squashrepeater/ruby/squash_ruby.rb
@@ -1,5 +1,8 @@
 require "squash/ruby"
 
+#FUTURE(willjr): Look into using hooks, to try to decouple the two parts of the lib
+# module Squash::Ruby
+
 # Monkey-patch Squash::Ruby so that instead of immediately sending Squash
 # notifications, capture the HTTP data in the local beanstalk/backburner queue
 module Squash::Ruby
@@ -14,24 +17,3 @@ module Squash::Ruby
     end
   end
 end
-
-
-#FUTURE(willjr): Look into using hooks, to try to decouple the two parts of the lib
-# module Squash::Ruby
-
-#   # You need to set a configuration :http_transmit_hook
-#   # This is supposed to contain lambdas/procs that will
-
-
-#   class << self
-#     private
-
-#     alias :http_transmit__original :http_transmit
-
-#     def http_transmit(url, headers, body)
-#       no_hook = lambda {|*args| self.http_transmit__original(*args) }
-#       http_transmit_hook = configuration(:http_transmit_hook) || no_hook
-#       http_transmit_hook.call(url, headers, body)
-#     end
-#   end
-# end
