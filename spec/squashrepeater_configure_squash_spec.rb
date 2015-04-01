06b589c0969cea0d649186c3449268cf32e4e094
mexisme <wildjim+github.com@kiwinet.org>
2015-04-01 15:40:22 +1300

    Tests

--- spec/squashrepeater_configure_squash_spec.rb
+++ spec/squashrepeater_configure_squash_spec.rb
@@ -5,15 +5,18 @@ describe SquashRepeater::Ruby::Configuration::Squash do
   let(:squash_ruby) { class_double("Squash::Ruby").as_stubbed_const(:transfer_nested_constants => true) }
 
   it do
-    expect(squash_ruby).to receive(:configure).with({ :set_this => "Set this" })
-    expect(squash_ruby).to receive(:configuration).with(:set_this)
-    config.set_this = "Set this"
+    [:set_this, :MosieAlong, :f00b4r].each do |key|
+      expect(squash_ruby).to receive(:configure).with({ key => key.to_s })
+      expect(squash_ruby).to receive(:configuration).with(key)
+      config.send("#{key.to_s}=".to_sym, key.to_s)
+    end
   end
 
-  it do
-    expect(squash_ruby).to receive(:configure).with({ :set_this => "Set this" })
-    expect(squash_ruby).to receive(:configuration).with(:set_this)
-    config.set_this = "Set this"
-    expect(config.set_this).to equal "Set this"
+  it "when setting and getting a Squash config attr" do
+    [:set_this, :MosieAlong, :f00b4r].each do |key|
+      expect(squash_ruby).to receive(:configuration).with(key).and_return(key.to_s)
+      #expect { config.api_key = "Set this" }.to eq "Set this"
+      expect(config.send(key)).to eq key.to_s
+    end
   end
 end
