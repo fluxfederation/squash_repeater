a807b39aa9946f84982fef0ae050b5d0216f09bd
mexisme <wildjim+github.com@kiwinet.org>
2015-04-02 14:16:09 +1300

    Remove unnecessary args for #method_missing

--- lib/squashrepeater/ruby/configure/squash.rb
+++ lib/squashrepeater/ruby/configure/squash.rb
@@ -11,15 +11,16 @@ class SquashRepeater::Ruby::Configuration::Squash
     return @configuration
   end
 
-  def method_missing(method_sym, *args, &p)
+  def method_missing(method_sym, val=nil)
     fail "Can't extract a meaningful name from the provided method_sym" unless method_sym.to_s =~ /^(.+?)=?$/
-    key, val = $1.to_sym, args[0]
+    key = $1.to_sym
 
     if method_sym.to_s =~ /=$/
       # Setter
       Squash::Ruby.configure({ key => val })
     end
     # Getter
+    #NB: Always return the value for the key
     Squash::Ruby.configuration(key)
   end
 end
