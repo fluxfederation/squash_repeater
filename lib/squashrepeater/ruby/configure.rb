36675d3c1a86849194cb73e9f6a8073c3cfdec5b
mexisme <wildjim+github.com@kiwinet.org>
2015-04-02 14:15:38 +1300

    Remove unnecessary wrapping/repetition

--- lib/squashrepeater/ruby/configure.rb
+++ lib/squashrepeater/ruby/configure.rb
@@ -12,15 +12,13 @@ module SquashRepeater::Ruby
   end
 
   class Configuration
-    # Squash configuration is private:
-    attr_accessor :squash_url, :squash_key, :squash_environment
-
     def initialize
+      self.logger = Logger.new(STDOUT)
+
       backburner do |c|
         # The nature of SquashRepeater is that a tiny local queueing system
         # captures the Squash notification, and retransmits it from a worker.
         # Therefore, we assume beanstalkd is running locally:
-
         c.beanstalk_url = "beanstalk://localhost"
         #c.beanstalk_url = "beanstalk://127.0.0.1"
         c.tube_namespace   = "squash-repeater"
@@ -28,79 +26,9 @@ module SquashRepeater::Ruby
         # NB: This relies on forking behaviour!
         c.default_worker = Backburner::Workers::Forking
 
-        #TODO: Choose a better failure mode:
-        #c.on_error = lambda { |ex| Airbrake.notify(ex) }
-
-        c.logger = Logger.new(STDOUT)
+        #c.on_error = lambda { |ex| Airbrake.notify(ex) }  #FUTURE: Choose a better failure mode:
       end
     end
-
-    def squash_url
-      squash.api_host
-    end
-
-    def squash_url=(value)
-      squash.api_host = value
-    end
-
-    def squash_key
-      squash.api_key
-    end
-
-    def squash_key=(value)
-      squash.api_key = value
-    end
-
-    def squash_environment
-      squash.environment
-    end
-
-    def squash_environment=(value)
-      squash.environment = value
-    end
-    alias :environment :squash_environment
-    alias :environment= :squash_environment=
-
-    def squash_disabled
-      squash.disabled
-    end
-
-    def squash_disabled=(value)
-      squash.disabled = value
-    end
-    alias :disabled :squash_disabled
-    alias :disabled= :squash_disabled=
-
-    def queue_host=(value)
-      backburner.beanstalk_url = "beanstalk://#{value}"
-    end
-
-    def queue_host
-      backburner.beanstalk_url.sub(%r(^beanstalk://), "")
-    end
-
-    def queue_host=(value)
-      backburner.beanstalk_url = "beanstalk://#{value}"
-    end
-
-    def namespace
-      backburner.tube_namespace
-    end
-
-    def namespace=(value)
-      backburner.tube_namespace = value
-    end
-
-    def logger
-      backburner.logger
-    end
-
-    def logger=(value)
-      # Squash doesn't allow you to use a different logger
-      backburner.logger = value
-    end
-
-    private
 
     def backburner(&p)
       if block_given?
@@ -117,9 +45,19 @@ module SquashRepeater::Ruby
         SquashRepeater::Ruby::Configuration::Squash.configuration
       end
     end
+
+    def loggers
+      [backburner.logger]  #FUTURE: Can we somehow get a Squash logger for this?
+    end
+
+    def logger=(value)
+      # Squash doesn't allow you to use a different logger
+      backburner.logger = value  #FUTURE: Can we somehow set a Squash logger for this?
+    end
   end
 end
 
+# This class relies on the class hierarchy having been created (above):
 require "squashrepeater/ruby/configure/squash"
 
 # Set the defaults:
