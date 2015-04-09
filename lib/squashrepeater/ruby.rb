889de83ab29fec603c29770477f9537c2431f592
mexisme <wildjim+github.com@kiwinet.org>
2015-04-09 18:09:27 +1200

    Add base SquashRepeater::Error class

--- lib/squashrepeater/ruby.rb
+++ lib/squashrepeater/ruby.rb
@@ -1,6 +1,10 @@
 # Configuration methods are in `configure.rb`
 # Exception capture and re-run are in `exception_queue.rb`
 
+module SquashRepeater
+  class Error < RuntimeError; end
+end
+
 require "squashrepeater/ruby/version"
 require "squashrepeater/ruby/configure"
 require "squashrepeater/ruby/exception_queue"
