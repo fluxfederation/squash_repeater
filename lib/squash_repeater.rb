1f4fd2b29987a424cc763346609822b672beed25
mexisme <wildjim+github.com@kiwinet.org>
2015-04-14 14:42:27 +1200

    Forgot Error class required by other libs

--- lib/squash_repeater.rb
+++ lib/squash_repeater.rb
@@ -1,6 +1,10 @@
 # Configuration methods are in `configure.rb`
 # Exception capture and re-run are in `exception_queue.rb`
 
+module SquashRepeater
+  class Error < RuntimeError; end
+end
+
 require "squash_repeater/version"
 require "squash_repeater/configure"
 require "squash_repeater/exception_queue"
@@ -8,5 +12,3 @@ require "squash_repeater/squash_ruby"
 
 # For Rails generators (only if Rails is used):
 require "generators/squash_repeater/install_generator" if defined? Rails
-
-class SquashRepeater::Error < RuntimeError; end
