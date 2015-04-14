b98e6740a8edd08ae305151d5b536bbfdcbcc241
mexisme <wildjim+github.com@kiwinet.org>
2015-04-14 14:25:40 +1200

    Make Rails generator work

--- lib/generators/squash_repeater/install_generator.rb
+++ lib/generators/squash_repeater/install_generator.rb
@@ -3,7 +3,7 @@ require "rails/generators"
 module SquashRepeater
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path("../../../templates", __FILE__)
+      source_root File.expand_path("../../templates", __FILE__)
       desc "Creates SquashRepeater initializer for your application"
 
       def copy_initializer
--- lib/squash_repeater.rb
+++ lib/squash_repeater.rb
@@ -9,3 +9,6 @@ require "squash_repeater/version"
 require "squash_repeater/configure"
 require "squash_repeater/exception_queue"
 require "squash_repeater/squash_ruby"
+
+# For Rails generators (only if Rails is used):
+require "generators/squash_repeater/install_generator" if defined? Rails
